d72da0d60170bdbab7ac27f43cd2ea310f1639d4
kaiserkey <kaiserkey2@gmail.com>
2019-11-19 17:01:55 -0300

    actualizar

--- src/Modelo/tipoInmuebleData.java
+++ src/Modelo/tipoInmuebleData.java
@@ -1,26 +1,11 @@
 
 package Modelo;
 
-import java.sql.Connection;
-import java.util.ArrayList;
-
 /**
  *
  * @kaiserkey
  * 
  */
 public class tipoInmuebleData {
-    private Connection con;
-    private ArrayList<tipoInmueble> tipos;
-    
-    public tipoInmuebleData(ConexionD con){
-        this.con = con.getConexion();
-        if (con == null){
-            System.out.println("ERROR la conexion no se ha establecido en tipoInmuebleData");
-        }else{
-            System.out.println("tipoInmuebleData Conexion establecida");
-        }
-    }
-    
     
 }
