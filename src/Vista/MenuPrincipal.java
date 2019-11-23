3cd3e0f3e3527c350125349efc21230f93eadae5
kaiserkey <56778742+kaiserkey@users.noreply.github.com>
2019-11-23 14:54:30 -0300

    Vista inmueble

--- src/Vista/MenuPrincipal.java
+++ src/Vista/MenuPrincipal.java
@@ -7,14 +7,14 @@ package Vista;
  * 
  */
 public class MenuPrincipal extends javax.swing.JFrame {
-    
+    private InmuebleVista inmuebleVista;
     /**
      * Creates new form VistaInmobiliaria
      */
     public MenuPrincipal() {
         initComponents();
         setTitle("Inmobiliaria");
-        setSize(1280,720);
+        setSize(800,600);
     }
 
 
@@ -34,6 +34,8 @@ public class MenuPrincipal extends javax.swing.JFrame {
         mContrato = new javax.swing.JMenuItem();
         menuPropietario = new javax.swing.JMenu();
         mPropietario = new javax.swing.JMenuItem();
+        menuTipoInmueble = new javax.swing.JMenu();
+        mTipoInmueble = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,13 +55,18 @@ public class MenuPrincipal extends javax.swing.JFrame {
         menu0.setText("Archivo");
 
         mSalir.setText("Salir");
+        mSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mSalirActionPerformed(evt);
+            }
+        });
         menu0.add(mSalir);
 
         bMenu.add(menu0);
 
         menuInmueble.setText("Inmueble");
 
-        mInmueble.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/finddoc.png"))); // NOI18N
+        mInmueble.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Img/casa.png"))); // NOI18N
         mInmueble.setText("Inmueble");
         mInmueble.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -97,6 +104,13 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
         bMenu.add(menuPropietario);
 
+        menuTipoInmueble.setText("Tipo de Inmueble");
+
+        mTipoInmueble.setText("Tipo de Inmueble");
+        menuTipoInmueble.add(mTipoInmueble);
+
+        bMenu.add(menuTipoInmueble);
+
         setJMenuBar(bMenu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -120,7 +134,12 @@ public class MenuPrincipal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void mInmuebleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mInmuebleActionPerformed
-
+            Escritorio.removeAll();
+            Escritorio.repaint();
+            inmuebleVista=new InmuebleVista();
+            inmuebleVista.setVisible(true);
+            Escritorio.add(inmuebleVista);
+            Escritorio.moveToFront(inmuebleVista);
     }//GEN-LAST:event_mInmuebleActionPerformed
 
     private void mPropietarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mPropietarioActionPerformed
@@ -130,7 +149,44 @@ public class MenuPrincipal extends javax.swing.JFrame {
         
     }//GEN-LAST:event_mPropietarioActionPerformed
 
+    private void mSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mSalirActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_mSalirActionPerformed
 
+        /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MenuPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MenuPrincipal().setVisible(true);
+            }
+        });
+    }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -141,10 +197,12 @@ public class MenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem mInquilino;
     public javax.swing.JMenuItem mPropietario;
     public javax.swing.JMenuItem mSalir;
+    private javax.swing.JMenuItem mTipoInmueble;
     public javax.swing.JMenu menu0;
     public javax.swing.JMenu menuContrato;
     public javax.swing.JMenu menuInmueble;
     public javax.swing.JMenu menuInquilino;
     private javax.swing.JMenu menuPropietario;
+    private javax.swing.JMenu menuTipoInmueble;
     // End of variables declaration//GEN-END:variables
 }
